815cc0e8143e0f695f79da82d54ceabdcfc85b2c
hansAndrianiaina <rasanssian@gmail.com>
2026-07-23 22:18:50 +0300

    EDIT to add default scene at the center and add background

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,16 +1,18 @@
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Environment, Stats } from '@react-three/drei'
+import { OrbitControls, Environment, Stats, Center, Bounds  } from '@react-three/drei'
 import { Suspense } from 'react'
+import { Model } from './Model'
 
 export default function Scene() {
   return (
     <Canvas camera={{ position: [0, 1, 4], fov: 45 }}>
+      <color attach="background" args={['#abbbb8']} />
       <Suspense fallback={null}>
-        {/* Replace this with <Model /> once you run gltfjsx on your .glb */}
-        <mesh>
-          <boxGeometry args={[1, 1, 1]} />
-          <meshStandardMaterial color="orange" />
-        </mesh>
+        <Bounds fit clip observe margin={1.2}>
+          <Center>
+            <Model />
+          </Center>
+        </Bounds>
         <Environment preset="city" />
       </Suspense>
       <OrbitControls />
