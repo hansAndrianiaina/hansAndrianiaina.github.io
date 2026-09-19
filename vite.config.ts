b36361553c623e39c55e4017030ebb5242048954
hansAndrianiaina <rasanssian@gmail.com>
2026-09-19 23:22:00 +0300

    EDIT to change vite config to include a preview in /dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Check if the current GitHub Action branch is 'dev'
+const isDevBranch = process.env.GITHUB_REF_NAME === 'dev'
+
 export default defineConfig({
-  base: '/',
+  base: isDevBranch ? '/dev' : '/',
   plugins: [react()],
 })
